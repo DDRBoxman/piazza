81a27914fa27d8cf119b675a66b99a72ff73f01e
Colin Edwards <colin@recursivepenguin.com>
2016-06-04 02:00:29 -0500

    Playing around with livestreamer for osx

---
+++ vlctest.sh
@@ -0,0 +1,1 @@
+livestreamer --player-args "--file-caching=5000 --no-video-deco --no-embedded-video --no-video-title-show --ignore-config --no-macosx-fspanel {filename}" https://www.twitch.tv/geekandsundry best
